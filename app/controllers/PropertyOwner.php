f0819a0e42df55d39622f18242756ef2e657dd1f
UltraKapes <kapes1980@hotmail.com>
2015-10-08 21:20:59 +1000

    Calendar part working

--- app/controllers/PropertyOwner.php
+++ app/controllers/PropertyOwner.php
@@ -40,6 +40,38 @@ class PropertyOwner extends Controller
     if (!Owner::isAuthenticated()) {
       $this->redirect('/');
     } else {
+
+      $this->setJavascriptDependencies([
+
+          WEBDIR . '/dzscalendar/dzscalendar.js',
+          WEBDIR . '/js/sweetalert.min.js',
+          WEBDIR . '/bootstrap/bootstrap.js',
+          WEBDIR . '/clockpicker/js/bootstrap.min.js',
+          WEBDIR . '/clockpicker/js/timepicki.js'
+
+      ]);
+
+      $this->setCSSDependencies([
+
+             WEBDIR . '/css/bootstrap.css',
+           //WEBDIR . '/style/style.css',
+          'http://fonts.googleapis.com/css?family=Carrois+Gothic',
+           WEBDIR . '/dzstooltip/dzstooltip.css',
+           WEBDIR . '/dzscalendar/dzscalendar.css',
+          'http://fonts.googleapis.com/css?family=Open+Sans',
+           // WEBDIR . '/clockpicker/css/bootstrap.css',
+
+          WEBDIR . '/css/sweetalert.css',
+          WEBDIR . '/css/wallfly.css',
+          WEBDIR . '/css/module.css',
+          WEBDIR . '/clockpicker/css/timepicki.css'
+
+
+
+
+      ]);
+
+
       $this->view('owner/calendar');
     }
   }
