db5a580779f34d35c9d6a7618eee7e230618603d
Khalfan AlFalasi <AlFalasi95@Khalfans-MacBook-Pro.local>
2015-10-13 02:58:08 +1000

    fixed tenant repair request page style for video

--- app/views/tenant/repairrequest.php
+++ app/views/tenant/repairrequest.php
@@ -6,15 +6,18 @@
  * Time: 4:51 PM
  */
 require_once '../app/views/templates/interfaceStart.php';
-require_once '../app/views/templates/selectProperty.php';
+//require_once '../app/views/templates/selectProperty.php';
 ?>
     <!--Content here-->
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="wlcm-h1">Make a Repair Request</h1>
-            </div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="page_heading">
+            <p>Repair Request</p>
+            <hr />
         </div>
+    </div>
+</div>
+
         <div class="row">
             <div class="col-md-4">
                 <div class="container-fluid">
@@ -35,6 +38,7 @@ require_once '../app/views/templates/selectProperty.php';
                             <div class="form-field">
                                 <label for="severity">Severity Level</label>
                                 <select name="severity" class="form-control formSelect">
+                                    <option value="" class="form-control">Please select...</option>
                                     <option value="low" class="form-control">Low</option>
                                     <option value="medium" class="form-control">Medium</option>
                                     <option value="high" class="form-control">High</option>
@@ -60,7 +64,7 @@ require_once '../app/views/templates/selectProperty.php';
                 </div>
             </div>
         </div>
-    </div>
+
 
 <?php
 require_once '../app/views/templates/interfaceEnd.php';
