86e0a8aaec499be27f89fe22653b630b12b1cebf
coderay1100 <rayp1100@gmail.com>
2015-10-15 12:55:31 +1000

    add modal form

--- app/views/owner/managedetails.php
+++ app/views/owner/managedetails.php
@@ -64,7 +64,10 @@ require_once '../app/views/templates/interfaceStart.php';
                         Tenant: - <br>
                     <?php endif ?>
 
-                    <a href="<?=WEBDIR?>/propertyowner/editproperty" class="btn btn-default">Edit</a> <a href="#" class="btn btn-success">Assign a tenant</a>
+
+
+                    <a href="<?=WEBDIR?>/propertyowner/editproperty" class="btn btn-default">Edit</a>
+                    <button class="btn btn-success" id="triggermodal" data-toggle="modal" data-target="#tenantForm">Assign a tenant</button>
                 <?php endif ?>
             </div>
             <div role="pillpanel" class="pill-pane" id="documents">2</div>
@@ -74,18 +77,49 @@ require_once '../app/views/templates/interfaceStart.php';
     </div>
 </div>
 
+<div class="row">
+    <div class="col-md-12">
+        <!-- Modal -->
+        <div class="modal fade" id="tenantForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+              </div>
+              <div class="modal-body">
+                <!-- FORM STARTS HERE -->
+                <form>
+                    
+                    <div class="form-field">
+                        <label for="firstname">First name</label>
+                        <input type="text" class="form-control" id="firstname" name="firstname">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="lastname">Last name</label>
+                        <input type="text" class="form-control" id="lastname" name="lastname">
+                    </div>
+
+                </form>
+                <!-- FORM ENDS HERE -->
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
 
 
 
 
 <script type="text/javascript">
     
-    $('.manage_properties_pills ul li').click(function (e) {
-        e.preventDefault()
-        $(this).pill('show')
-    })
     
-    document.title = 'Properties - WallFly';
 </script>
     
 <?php
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,11 +1,14 @@
 <!-- Bootstrap Core JavaScript -->
 <script src="<?=WEBDIR?>/js/bootstrap.min.js"></script>
+
+<script src="<?=WEBDIR?>/js/managedetails.js"></script>
 <!-- Plugin JavaScript -->
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<!-- <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script> -->
 <script src="<?=WEBDIR?>/js/classie.js"></script>
 <script src="<?=WEBDIR?>/js/cbpAnimatedHeader.js"></script>
 <!-- Custom Theme JavaScript -->
 <script src="<?=WEBDIR?>/js/wallfly.js"></script>
 <script src="<?=WEBDIR?>/js/offcanvas.js"></script>
+
 </body>
 </html>
