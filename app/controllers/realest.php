85218be2b9984aa8c7abf1752b725c49c6c8d1dd
coderay1100 <rayp1100@gmail.com>
2015-10-26 00:14:30 +1000

    removed real estate password constraint

--- app/controllers/realest.php
+++ app/controllers/realest.php
@@ -19,15 +19,6 @@ class RealEst extends Controller
       // TODO validate data
       if ($_POST['password'] !== $_POST['passwordrepeat']) {
         Flash::set('error_message', 'Your passwords did not match!');
-      } elseif (!preg_match("#[0-9]+#", $_POST['password'])) {
-        // should contain at least 1 number
-        Flash::set('error_message', 'Your password should contain at least 1 number');
-      } elseif (!preg_match("#[A-Z]+#", $_POST['password'])) {
-        // should contain at least 1 capital letter
-        Flash::set('error_message', 'Your password should contain at least an uppercase letter');
-      } elseif (!preg_match("#[a-z]+#", $_POST['password'])) {
-        // should contain at least 1 lowercase letter
-        Flash::set('error_message', 'Your password should contain at least a lowercase letter');
       } else {
         // TODO submit data
         Real_Estate::create([
