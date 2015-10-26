5adcc539d9310a9591c2830ed4b643f144b193cc
jimmy kovacevic <yojimmbo@hotmail.com>
2015-10-26 21:03:42 +1000

    Agent index page styled

--- app/views/agent/index.php
+++ app/views/agent/index.php
@@ -1,134 +1,103 @@
 <?php
-    require_once '../app/views/templates/interfaceStartIndex.php';
+require_once '../app/views/templates/interfaceStartIndex.php';
 ?>
-
-<script src="/wallfly-mvc/public/js/dropdown/dropdown.js"></script>
-<link href="/wallfly-mvc/public/js/dropdown/dropdown.css" rel="stylesheet">
-          
 
 <!--Content here-->
 <div class="row">
     <div class="col-md-12">
-        <h1 class="wlcm-h1">Welcome <span class="user-color"><?php echo $_SESSION['user']->firstname?>!
-             <button class="btn btn-default" data-toggle="modal" data-target="#addpropertyform">+ Add property</button></span></h1>
+        <div class="col-md-8">
+            <h1 class="wlcm-h1">Welcome <span class="user-color"><?php echo $_SESSION['user']->firstname?>!</span></h1>
+        </div>
+        <div class="col-md-4">
+            <button class="btn btn-add-property pull-right" data-toggle="modal" data-target="#addpropertyform"><i class="fa fa-plus-square"></i>Add property</button>
+        </div>
     </div>
 </div>
+</div>
+</div>
 
-<div class="row">
+<div class="row bottom-section">
     <div class="col-md-12">
-        <section id="select_property">
-
-            <!-- create address dropdown list only if agent or owner usertype -->
+        <div class="row dash_p_list">
             <?php
-
-                  $properties = $_SESSION['user']->getProperties();
-
-
+            $properties = $_SESSION['user']->getProperties();
+            $count = 0;
+            foreach ($properties as $property) {
+                ?>
+                <div class="col-md-3">
+                    <form id="viewDetails" method="post" action="<?=WEBDIR?>/propertyagent/viewDetails">
+                        <div class="dash_p">
+                            <div class="dash_p_img">
+                                <img class="img-responsive" src="<?php echo $property->photo?>">
+                            </div>
+                            <div class="dash_p_add_btn">
+                                <div class="dash_p_add">
+                                    <p><?php echo $property->address?></p>
+                                </div>
+                                <div class="dash_p_btn">
+                                    <button type="submit" name="submit" value="<?php echo $count;?>" class="btn btn-view-details">View Details</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <?php
+                $count++;
+            }
             ?>
-            <div class="select-property-dropdown">
-<!--                 <h3>Please select a property:</h3>-->
-                <select class="ui search dropdown">
-                    <option value="">Select a property...</option>
-                    <?php
-                for($i=0;$i<count($properties);$i++){
-                    $selected = '';
-                    echo value;
-                    if ($properties[$i]->id === $pID) {
-                        $selected = 'selected';
-                    }
-                    echo '<option value="'.$i.'" ' . $selected . '>' . $properties[$i]->address . '</option>';
-
-                } ?>
-                </select>
-
-            </div>
-        </section>
+        </div>
     </div>
-</div>  
-    </div>
-</div>  
-
-
-    <!-- add property modal -->
-    <div class="modal modal-vcenter fade" id="addpropertyform" tabindex="-1" role="dialog"  aria-labelledby="mySmallModalLabel">
+</div>
+<!-- add property modal -->
+<div class="modal modal-vcenter fade" id="addpropertyform" tabindex="-1" role="dialog"  aria-labelledby="mySmallModalLabel">
     <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
                 <p class="modal-title">Add Property</p>
             </div>
-
-              <div class="modal-body">
-               
+            <div class="modal-body">
                 <!-- FORM STARTS HERE -->
-                <form action="<?=WEBDIR?>/propertyagent/addproperty" method="post">            
-                        <div class="add_property">
+                <form action="<?=WEBDIR?>/propertyagent/addproperty" method="post">
+                    <div class="add_property">
                         <div class="ap_field_a">
                             <label for="address">Address</label>
                             <input type="text" class="form-control" id="address" name="address" required>
                         </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                        <div class="ap_field_r">
-                            <label for="rent_amount">Rent amount ($)</label>
-                            <input type="text" class="form-control" id="rent_amount" name="rent_amount" required>
-                        </div></div>
+                        <div class="row">
                             <div class="col-md-6">
-                        <div class="ap_field_ps">
-                            <label for="payment_schedule">Payment schedule</label>
-                            <select name="payment_schedule" id="payment" class="form-control">      
-                                <option value="" hidden>Please select...</option>
-                                <option value="WEEKLY">Weekly</option>
-                                <option value="FORTNIGHTLY">Fortnightly</option>
-                                <option value="MONTHLY">Monthly</option>
-                            </select>
+                                <div class="ap_field_r">
+                                    <label for="rent_amount">Rent amount ($)</label>
+                                    <input type="text" class="form-control" id="rent_amount" name="rent_amount" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="ap_field_ps">
+                                    <label for="payment_schedule">Payment schedule</label>
+                                    <select name="payment_schedule" id="payment" class="form-control">
+                                        <option disabled selected hidden>Please select...</option>
+                                        <option value="WEEKLY">Weekly</option>
+                                        <option value="FORTNIGHTLY">Fortnightly</option>
+                                        <option value="MONTHLY">Monthly</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
-                        </div>
-                    </div>
-                            
                         <div class="col-md-12">
                             <div class="ap_btnz">
-                       <button type="submit" class="btn btn-save-changes pull-right">Save changes</button>
-                        </div>
+                                <button type="submit" class="btn btn-save-changes pull-right">Save changes</button>
+                            </div>
                         </div>
                     </div>
-                     
                 </form>
                 <!-- FORM ENDS HERE -->
             </div>
         </div>
     </div>
 </div>
-
 <script>
-        $('.ui.search.dropdown').dropdown({
-            fullTextSearch: true, 
-            sortSelect: true, 
-            match:'text',
-            onChange: function(value) {
-                var arraypos = value;
-                jQuery.ajax({
-                    url: '/wallfly-mvc/public/dashboard/selectedProperty',
-                    type: "POST",
-                    data: {
-                        selected: arraypos
-                    },
-                    success: function (result) {
-                        window.location.reload();
-                    },
-                    error: function (result) {
-                        alert('Exeption:' + exception);
-                    }
-                });
-            }
-
-    });
-    
- document.title = 'Dashboard - WallFly';
-
+    document.title = 'Dashboard - WallFly';
 </script>
-
-
 <?php
 require_once '../app/views/templates/interfaceEnd.php';
 ?>
--- app/views/owner/index.php
+++ app/views/owner/index.php
@@ -15,106 +15,85 @@
 </div>
 </div>
 </div>
-
 <div class="row bottom-section">
     <div class="col-md-12">
         <div class="row dash_p_list">
-      
-        <!-- create address dropdown list only if agent or owner usertype -->
-              
-        <?php
-        $properties = $_SESSION['user']->getProperties();
-        $count = 0;
-        foreach ($properties as $property) {
-        ?>
-        
-          
-            <div class="col-md-3">
-                         
-             
-                <form id="viewDetails" method="post" action="<?=WEBDIR?>/propertyowner/viewDetails">
-                    
-             <div class="dash_p">
-                    <div class="dash_p_img">
-                    <img class="img-responsive" src="<?php echo $property->photo?>">
-                    </div>
-                    <div class="dash_p_add_btn">
-                    <div class="dash_p_add">
-                    <p><?php echo $property->address?></p>
-                    </div>
-                 <div class="dash_p_btn">
-                    <button type="submit" name="submit" value="<?php echo $count;?>" class="btn btn-view-details">View Details</button>
-                    </div>
-                   </div>
-                 </div>
-                </form>
-            
-                     </div>
-                             
-       
-        <?php
-            $count++;
-        }
-        ?>
-               
-      </div>      
-        
+            <?php
+            $properties = $_SESSION['user']->getProperties();
+            $count = 0;
+            foreach ($properties as $property) {
+                ?>
+                <div class="col-md-3">
+                    <form id="viewDetails" method="post" action="<?=WEBDIR?>/propertyowner/viewDetails">
+                        <div class="dash_p">
+                            <div class="dash_p_img">
+                                <img class="img-responsive" src="<?php echo $property->photo?>">
+                            </div>
+                            <div class="dash_p_add_btn">
+                                <div class="dash_p_add">
+                                    <p><?php echo $property->address?></p>
+                                </div>
+                                <div class="dash_p_btn">
+                                    <button type="submit" name="submit" value="<?php echo $count;?>" class="btn btn-view-details">View Details</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <?php
+                $count++;
+            }
+            ?>
+        </div>
     </div>
-</div>  
- 
-
-
-    <!-- add property modal -->
-    <div class="modal modal-vcenter fade" id="addpropertyform" tabindex="-1" role="dialog"  aria-labelledby="mySmallModalLabel">
+</div>
+<!-- add property modal -->
+<div class="modal modal-vcenter fade" id="addpropertyform" tabindex="-1" role="dialog"  aria-labelledby="mySmallModalLabel">
     <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
                 <p class="modal-title">Add Property</p>
             </div>
-
-              <div class="modal-body">
-               
+            <div class="modal-body">
                 <!-- FORM STARTS HERE -->
-                <form action="<?=WEBDIR?>/propertyowner/addproperty" method="post">            
-                        <div class="add_property">
+                <form action="<?=WEBDIR?>/propertyowner/addproperty" method="post">
+                    <div class="add_property">
                         <div class="ap_field_a">
                             <label for="address">Address</label>
                             <input type="text" class="form-control" id="address" name="address" required>
                         </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                        <div class="ap_field_r">
-                            <label for="rent_amount">Rent amount ($)</label>
-                            <input type="text" class="form-control" id="rent_amount" name="rent_amount" required>
-                        </div></div>
+                        <div class="row">
                             <div class="col-md-6">
-                        <div class="ap_field_ps">
-                            <label for="payment_schedule">Payment schedule</label>
-                            <select name="payment_schedule" id="payment" class="form-control">      
-                                <option disabled selected hidden>Please select...</option>
-                                <option value="WEEKLY">Weekly</option>
-                                <option value="FORTNIGHTLY">Fortnightly</option>
-                                <option value="MONTHLY">Monthly</option>
-                            </select>
+                                <div class="ap_field_r">
+                                    <label for="rent_amount">Rent amount ($)</label>
+                                    <input type="text" class="form-control" id="rent_amount" name="rent_amount" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="ap_field_ps">
+                                    <label for="payment_schedule">Payment schedule</label>
+                                    <select name="payment_schedule" id="payment" class="form-control">
+                                        <option disabled selected hidden>Please select...</option>
+                                        <option value="WEEKLY">Weekly</option>
+                                        <option value="FORTNIGHTLY">Fortnightly</option>
+                                        <option value="MONTHLY">Monthly</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
-                        </div>
-                    </div>
-                            
                         <div class="col-md-12">
                             <div class="ap_btnz">
-                       <button type="submit" class="btn btn-save-changes pull-right">Save changes</button>
-                        </div>
+                                <button type="submit" class="btn btn-save-changes pull-right">Save changes</button>
+                            </div>
                         </div>
                     </div>
-                     
                 </form>
                 <!-- FORM ENDS HERE -->
             </div>
         </div>
     </div>
 </div>
-
 <script>
     document.title = 'Dashboard - WallFly';
 </script>
